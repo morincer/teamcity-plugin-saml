efeebfcbe3e45e633e2c0eb1eaafc2788996d236
Andrey Makarov <morincer@gmail.com>
2019-08-21 13:50:14 +0300

    WIP: Implement attributes mapping for automatically created users. Change ugly radios to nice drop downs

--- saml-authentication-server/src/main/vue/admin-ui/src/components/SamlAttributeSelect.vue
+++ saml-authentication-server/src/main/vue/admin-ui/src/components/SamlAttributeSelect.vue
@@ -1,5 +1,11 @@
 <template>
     <div>
+        <select v-model="mappingType" v-on:change="emitChange()">
+            <option :value="SamlAttributeMappingTypes.None">None</option>
+            <option :value="SamlAttributeMappingTypes.NameID">Name ID</option>
+            <option :value="SamlAttributeMappingTypes.Other">Custom Attribute</option>
+        </select>
+        <!--
         <input type="radio" id="none" v-on:change="emitChange()" :value="SamlAttributeMappingTypes.None" v-model="mappingType"/>
         <label for="none">None</label>
 
@@ -8,8 +14,8 @@
 
         <input type="radio" id="custom" v-on:change="emitChange()" :value="SamlAttributeMappingTypes.Other" v-model="mappingType"/>
         <label for="custom">Custom</label>
-
-        <input type="text" v-on:change="emitChange()" v-if="mappingType == SamlAttributeMappingTypes.Other" v-model="customAttributeName"/>
+-->
+        <input type="text" style="margin-left: 10px" v-on:change="emitChange()" v-if="mappingType == SamlAttributeMappingTypes.Other" v-model="customAttributeName"/>
     </div>
 </template>
 
