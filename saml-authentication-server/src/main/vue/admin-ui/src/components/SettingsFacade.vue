0094d647ac5ae99436a276040236c62365c2d0f1
Andrey Makarov <morincer@gmail.com>
2019-08-30 18:15:01 +0300

    WIP: New connection wizard. Basic layout for Okta (no persistence yet)

---
+++ saml-authentication-server/src/main/vue/admin-ui/src/components/SettingsFacade.vue
@@ -0,0 +1,88 @@
+<template>
+    <div>
+        <div class="successMessage" v-if="settingsDecoration.introMessage" style="margin-bottom: 10px">{{settingsDecoration.introMessage}}
+        </div>
+        <RunnerForm :disabled="isLoading" v-for="group in settingsDecoration.groups">
+            <GroupingHeader>{{group.title}}
+                <ProgressIndicator title="...fetching current settings..." v-if="isLoading"/>
+            </GroupingHeader>
+
+            <tr v-if="group.message">
+                <td colspan="2">
+                    <span class="smallNote">{{group.message}}</span>
+                </td>
+            </tr>
+
+            <RunnerFormRow v-for="decoration in group.parameters">
+                <template v-slot:label>{{decoration.label}}</template>
+                <template v-slot:content>
+                    <TextInput v-model="settings[decoration.fieldName]" :textarea="decoration.textarea == true" v-if="decoration.readonly !== true"/>
+                    <span v-if="decoration.readonly === true">{{settings[decoration.fieldName]}}</span>
+                </template>
+                <template v-slot:note v-if="decoration.hint">{{decoration.hint}}</template>
+            </RunnerFormRow>
+        </RunnerForm>
+    </div>
+</template>
+
+<script lang="ts">
+    import {Component, Prop} from "vue-property-decorator";
+    import Vue from 'vue';
+    import {ISettingsApiService, SamlSettings} from "@/services/ISettingsApiService";
+    import RunnerForm from "@/components/RunnerForm.vue";
+    import RunnerFormRow from "@/components/RunnerFormRow.vue";
+    import GroupingHeader from "@/components/GroupingHeader.vue";
+    import TextInput from "@/components/TextInput.vue";
+    import {appConfig} from "@/main.dependencies";
+    import ProgressIndicator from "@/components/ProgressIndicator.vue";
+
+    export interface ParametersGroup {
+        message?: string,
+        title: string,
+        parameters: ParameterDecoration[]
+    }
+
+    export interface ParameterDecoration {
+        fieldName: string,
+        label: string,
+        hint: string,
+        textarea?: boolean,
+        readonly?: boolean,
+    }
+
+    export interface SettingsDecoration {
+        introMessage: string,
+        groups: ParametersGroup[]
+    }
+
+    @Component({components: {TextInput, GroupingHeader, RunnerForm, RunnerFormRow, ProgressIndicator}})
+    export default class SettingsFacade extends Vue {
+        @Prop()
+        settingsDecoration?: SettingsDecoration;
+
+        settings?: SamlSettings = {};
+
+        public settingsApiService: ISettingsApiService = appConfig.settingsApiService!;
+
+        isLoading: Boolean = false;
+
+        async mounted() {
+            try {
+                this.isLoading = true;
+                const result = await this.settingsApiService.get();
+
+                if (result.result) {
+                    this.settings = result.result;
+                }
+            } catch (e) {
+                this.settings = {};
+            } finally {
+                this.isLoading = false;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- saml-authentication-server/src/main/vue/admin-ui/src/views/NewConnectionWizard.vue
+++ saml-authentication-server/src/main/vue/admin-ui/src/views/NewConnectionWizard.vue
@@ -1,28 +1,72 @@
 <template>
     <div>
         <h3>Choose SSO Provider:</h3>
-        <select v-model="provider">
+        <select v-model="provider" style="margin-bottom: 20px">
             <option :value="SsoProvider.Okta">Okta</option>
             <option :value="SsoProvider.OneLogin">OneLogin</option>
             <option disabled>-----------------</option>
             <option :value="SsoProvider.ImportMetadata">Other - Import Metadata</option>
             <option :value="SsoProvider.None">Other - Exit and Setup Manually</option>
         </select>
+        <SettingsFacade v-if="provider == SsoProvider.Okta" :settingsDecoration="oktaDecoration" />
+        <RunnerForm>
+            <template v-slot:actions>
+                <input type="submit" value="Save" class="btn btn_primary submitButton"
+                       :disabled="isLoading"
+                       @click="submit()"/>
+
+                <input type="submit" value="Cancel" class="btn submitButton"
+                       :disabled="isLoading"
+                       @click="submit()"/>
+            </template>
+        </RunnerForm>
     </div>
 </template>
 
 <script lang="ts">
     import Vue from "vue";
     import {Component} from "vue-property-decorator";
+    import SettingsFacade, {SettingsDecoration} from "@/components/SettingsFacade.vue";
+    import {SamlSettings} from "@/services/ISettingsApiService";
+    import RunnerForm from "@/components/RunnerForm.vue";
 
     enum SsoProvider {
         None, Okta, OneLogin, ImportMetadata
     }
 
-    @Component({})
+    const OktaDecoration : SettingsDecoration = {
+        introMessage: "Login to Okta administration console. " +
+            "Click the Developer Console and choose Classic UI. Go to Applications. " +
+            "Choose (or create) the IdP application (i.e. Teamcity). ",
+        groups: [
+            {
+                title: "Okta Identity Provider Settings",
+                message: "Go to the Sign On tab and click 'View Setup Instructions' button. Fill the values into fields below.",
+                parameters: [
+                    { fieldName: "ssoEndpoint", hint: null, label: "Identity Provider Single Sign-On URL:" },
+                    { fieldName: "issuerUrl", hint: null, label: "Identity Provider Issuer:" },
+                    { fieldName: "publicCertificate", label: "X.509 Certificate:", textarea: true, hint: "Paste full X.509 certificate here" },
+                ],
+            },
+            {
+                title: "Teamcity Service Provider Settings",
+                message: "Go to the General tab -> SAML Settings and click 'Edit' button -> Next to the Configure SAML step. " +
+                    "Fill the values from fields below.",
+                parameters: [
+                    { fieldName: "ssoCallbackUrl", hint: null, label: "Single sign on URL:", readonly: true },
+                    { fieldName: "entityId", hint: null, label: "Audience URI (SP Entity ID):", readonly: true },
+                ],
+            }
+        ]
+    };
+
+    @Component({ components: { SettingsFacade, RunnerForm}})
     export default class NewConnectionWizard extends Vue {
         SsoProvider = SsoProvider;
-        provider: SsoProvider = this.SsoProvider.Okta
+        provider: SsoProvider = this.SsoProvider.Okta;
+        settings!: SamlSettings;
+        oktaDecoration = OktaDecoration;
+
     }
 
 </script>
