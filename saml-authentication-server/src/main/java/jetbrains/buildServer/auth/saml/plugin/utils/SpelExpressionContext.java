cf9900a96cf24db81909cb737db1d568bfc62361
Andrey Titov <andrey.titov@jetbrains.com>
2024-02-01 10:06:28 +0100

    Use SimpleEvaluationContext instead of StandardEvaluationContext

--- saml-authentication-server/src/main/java/jetbrains/buildServer/auth/saml/plugin/utils/SpelExpressionContext.java
+++ saml-authentication-server/src/main/java/jetbrains/buildServer/auth/saml/plugin/utils/SpelExpressionContext.java
@@ -3,17 +3,17 @@ package jetbrains.buildServer.auth.saml.plugin.utils;
 import com.onelogin.saml2.Auth;
 import lombok.var;
 import org.springframework.context.expression.MapAccessor;
-import org.springframework.expression.spel.support.StandardEvaluationContext;
+import org.springframework.expression.EvaluationContext;
+import org.springframework.expression.spel.support.SimpleEvaluationContext;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-public class SpelExpressionContext extends StandardEvaluationContext {
+public class SpelExpressionContext {
+    private final EvaluationContext evaluationContext;
 
     public SpelExpressionContext(Map<String, ?> attributes) {
-        setRootObject(attributes);
-        addPropertyAccessor(new MapAccessor());
+        evaluationContext = new SimpleEvaluationContext.Builder(new MapAccessor()).withInstanceMethods().withRootObject(attributes).build();
     }
 
     public SpelExpressionContext(Auth auth) {
@@ -31,11 +31,14 @@ public class SpelExpressionContext extends StandardEvaluationContext {
         attributes.put("nameid", auth.getNameId());
         attributes.put("lastassertionid", auth.getLastAssertionId());
 
-        setRootObject(attributes);
-        addPropertyAccessor(new MapAccessor());
+        evaluationContext = new SimpleEvaluationContext.Builder(new MapAccessor()).withRootObject(attributes).build();
     }
 
     public <T> Map<String, T> getRootObjectAsMap() {
-        return (Map<String, T>) getRootObject().getValue();
+        return (Map<String, T>) evaluationContext.getRootObject().getValue();
+    }
+
+    public EvaluationContext getEvaluationContext() {
+        return evaluationContext;
     }
 }
